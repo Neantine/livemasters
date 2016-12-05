43b794b51348c5225744b751fbee9e6cc560f042
Neantine <4sth4rtee@gmail.com>
2016-12-05 13:12:18 +0100

    update

--- wp-config.php
+++ wp-config.php
@@ -77,7 +77,10 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);define('FS_METHOD', 'direct');
+define('WP_DEBUG', true);
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+define('FS_METHOD', 'direct');
 
 /* That's all, stop editing! Happy blogging. */
 
