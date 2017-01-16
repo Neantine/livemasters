99333935c9437538cba081dfad74f214f938f6f7
Neantine <4sth4rtee@gmail.com>
2017-01-16 12:12:25 +0100

    removed rss from home page

--- wp-content/themes/fundify-child/content-project.php
+++ wp-content/themes/fundify-child/content-project.php
@@ -109,9 +109,4 @@ if ($project_id > 0) {
 	</div>
 </article>
 
-<?php if ( is_active_sidebar( 'home_right_1' ) ) : ?>
-	<div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-		<?php dynamic_sidebar( 'home_right_1' ); ?>
-	</div><!-- #primary-sidebar -->
-<?php endif; ?>
 
